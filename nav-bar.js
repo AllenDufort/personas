7f6d97521737dd2651dba7272242036bbbdc22d8
AllenDufort <allen_dufort@brown.edu>
2025-04-22 15:13:48 -0400

    added bootstrap

--- nav-bar.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    var x = document.getElementById("nav-bar");
-    if (x.className === "nav") {
-        x.className += " menu-clicked";
-    } else {
-        x.className = "nav";
-    }
-}
